0c81d50d9699064cf83117ba20b05d680fe0f74d
Sajad Ghawami <sajad.ghawami@codestra.io>
2021-05-09 13:41:12 +0200

    wip

--- public/root/injected.js
+++ public/root/injected.js
@@ -177,7 +177,7 @@ window.addEventListener("load", function (event) {
     const contactForm = $(
       '<iframe class="embedded-contact" width="100%" height="100%" src="https://recscout.hubspotpagebuilder.com/recscout-pr%C3%A4sentation" frameborder="0" style="height:100vh"></iframe>'
     );
-    contactAppend.append(contactForm);
+    contactAppend.after(contactForm);
   }
 
   // Auto accept Rating Page
