af21af04d386f709059c5e42582352a192616235
Sajad Ghawami <sajad.ghawami@codestra.io>
2021-06-07 15:51:30 +0200

    wip

--- public/root/injected.js
+++ public/root/injected.js
@@ -110,7 +110,7 @@ window.addEventListener("load", function (event) {
     $(".wrapper").css({ maxWidth: "100%" });
   } else {
     // add the sidebar if not in about
-    $(".left-navi ").css({ display: "inline-block" });
+    $(".left-navi").css({ display: "inline-block" });
   }
 
   if (isUserLoggedIn) {
@@ -124,6 +124,31 @@ window.addEventListener("load", function (event) {
   }
 
   // pages
+  // settings
+  const isSettingsPage =
+    splittedPathname[splittedPathnameArrayPosition] === "settings";
+  if (isSettingsPage) {
+    // add the links
+    $("#person_description").before($("<br/>"));
+    $("#person_description").before(
+      $(
+        "<a href='https://recscout.medium.com/musterprofil-personalberater-edcff48fe6f9' target='_blank'><small>Mustervorlage für Personalberater</small></a>"
+      )
+    );
+    $("#person_description").before($("<br/>"));
+    $("#person_description").before(
+      $(
+        "<a href='https://recscout.medium.com/musterprofil-unternehmen-58112506312b' target='_blank'><small>Mustervorlage für Unternehmen</small></a>"
+      )
+    );
+    // add the placeholder
+    $("#person_description").css({ height: "300px", resize: "auto" });
+    $("#person_description").prop(
+      "placeholder",
+      "Für Recruiter (Personalberater):\nIn Ihrem Profil stehen Sie als Person im Mittelpunkt. Skizzieren Sie Ihre Expertise als Personalberater, Ihren Kundenfokus sowie weitere relevante Spezialisierungen. Eine Mustervorlage finden Sie in dem Link über diesem Textfeld.\n\nFür Unternehmen:\nSie entscheiden selbst, wann und mit welchen  Personalberatern Sie in Kontakt treten.  Stellen Sie Ihr Unternehmen für den weiteren Verlauf kurz vor:                                Empfehlung:   „Über uns” Text der Unternehmenshomepage einfügen. Ihr Profil wird nicht veröffentlicht. Ihre Informationen werden nur für von Ihnen kontaktierte Recruiter sichtbar."
+    );
+  }
+
   // listing page, display rating if
   const isListingsPage =
     splittedPathname[splittedPathnameArrayPosition - 1] === "listings";
